196d7b205aae736781627533563b647c993744c0
ma-lijun <343190282@qq.com>
2017-10-22 20:20:16 +0800

    注销多余的url配置

--- df_user/urls.py
+++ df_user/urls.py
@@ -5,6 +5,6 @@ from df_user import views
 
 urlpatterns = [
     url('^register/$', views.register),
-    url(r'^register_handle/$', views.register_handle),  # 实现用户信息的注册
+    # url(r'^register_handle/$', views.register_handle),  # 实现用户信息的注册
     url(r'^check_user_exist/$', views.check_user_exist),  # 检查用户是否存在
 ]
