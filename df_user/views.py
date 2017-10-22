bd00b0bd3397f34a2b84fb3e61eada539c82e530
ma-lijun <343190282@qq.com>
2017-10-22 20:19:22 +0800

    优化注册页接口，增加登录装饰器

--- df_user/views.py
+++ df_user/views.py
@@ -4,11 +4,25 @@ from django.http import JsonResponse
 from django.core.mail import send_mail
 from django.conf import settings
 from df_user.tasks import register_success_send_mail
+from django.views.decorators.http import require_GET,require_POST,require_http_methods
 
-# Create your views here.
+
+# 使用django的内置装饰器功能，显示访问方式,优化register接口设计，删除多余接口register_handle
+@require_http_methods(['GET', 'POST'])
 def register(request):
     # 显示注册页面
-    return render(request, 'df_user/register.html')
+    if request.method == 'GET':
+        return render(request, 'df_user/register.html')
+    elif request.method == 'POST':
+        # 接受数据
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        # 将保存数据的方法提取到模型管理器类
+        Passport.objects.add_one_passport(username=username, password=password, email=email)
+        # # 3.给用户注册邮箱发邮件
+        register_success_send_mail.delay(username=username, password=password, email=email)
+        return redirect('/df_user/login/')
 
 
 def register_handle(request):
@@ -29,6 +43,8 @@ def register_handle(request):
     return redirect('/df_user/login/')
 
 
+@require_GET
+# /user/check_user_exist/   get 方式验证用户名是否存在
 def check_user_exist(request):
     # 接收用户
     username = request.GET.get('username')
