9f7f4c5809373ff7fed184b40ce229f308749d9e
ma-lijun <343190282@qq.com>
2017-10-28 14:18:55 +0800

    展示首页

--- df_goods/urls.py
+++ df_goods/urls.py
@@ -1,3 +1,10 @@
 # _*_ coding:utf-8 _*_ 
+from django.conf.urls import include, url
+from django.contrib import admin
+from df_goods import views
 
+urlpatterns = [
+    url(r'^$', views.home_list_page),  # 显示首页
+
+]
 
--- df_goods/views.py
+++ df_goods/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# //
+def home_list_page(request):
+    return render(request, 'df_goods/index.html')
