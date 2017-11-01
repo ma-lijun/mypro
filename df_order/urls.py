6c2e6760ff226ae59c57c88a68a3ca7a60fe8061
ma-lijun <343190282@qq.com>
2017-11-01 22:37:49 +0800

    购物车视图

--- df_order/urls.py
+++ df_order/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from df_order import views
 
 urlpatterns = [
-    url(r'^place/$', views.show_place), # 显示提交订单页面
-    url(r'^commit/$', views.order_commit), # 订单生成
+    url(r'^place/$', views.show_place),  # 显示提交订单页面
+    url(r'^commit/$', views.order_commit),  # 订单生成
 ]
